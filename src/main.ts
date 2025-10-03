0d2d96d07bb754076151d59933fa4c5f9e9d37ff
Angel <162243536+AngelCasta34@users.noreply.github.com>
2025-10-03 02:42:25 +0000

    removed comment

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,6 @@ const button = document.getElementById("increment")!;
 const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
-  // This looks like to a good place to add some logic!
   console.log("I have these thingies:", button, counterElement, counter);
   counter++;
   counterElement.textContent = counter.toString();
